3166182da2502d2b0f9fefc00263b469ff9ec649
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2025-04-15 09:31:51 +0100

    added the icons. and got them in line on the leftsidepanel added some extra styles to make them flex on column

--- src/components/LeftSidePanel.tsx
+++ src/components/LeftSidePanel.tsx
@@ -1,8 +1,16 @@
 // images and icons
 import Logo from "../assets/images/FORA_logomarque_black_1.png"
+import Icon from "../assets/icons/Icon.svg"
+import Calendar from "../assets/icons/Calendar.svg"
+import CheckIn from "../assets/icons/Check-in.svg"
+import LogOut from "../assets/icons/Log-out.svg"
+import Building from "../assets/icons/Building.svg"
 
 // react router
 import { Link } from "react-router-dom"
+
+// styles
+import "../styles/leftSidePanel.scss"
 
 const LeftSidePanel = () => {
   return (
@@ -10,6 +18,24 @@ const LeftSidePanel = () => {
       <Link to="/">
         <img src={Logo} alt="FORA Logo" />
       </Link>
+      <Link to="">
+        <img src={Icon} alt="Home Dashboard" className="icon" />
+      </Link>
+      <Link to="">
+        <img src={Calendar} alt="Calendar" className="icon" />
+      </Link>
+      <Link to="">
+        <img src={CheckIn} alt="QR Check In" className="icon" />
+      </Link>
+
+      <div>
+        <Link to="">
+          <img src={Building} alt="Building" className="icon" />
+        </Link>
+        <Link to="">
+          <img src={LogOut} alt="Log Out" className="icon" />
+        </Link>
+      </div>
     </nav>
   )
 }
