909742abba020ecc6704068200dc0f8e5436be0c
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2025-04-15 11:28:52 +0100

    added the dashboardheader component. starting on some styling now

---
+++ src/components/DashboardHeader.tsx
@@ -0,0 +1,15 @@
+// styles
+import "../styles/dashboardHeader.scss"
+
+const DashboardHeader = () => {
+  return (
+    <div className="dashboard-header">
+      <h2>Hey, Joe</h2>
+      <select className="building-select">
+        <option>Stanley Building</option>
+      </select>
+    </div>
+  )
+}
+
+export default DashboardHeader
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 // components
+import DashboardHeader from "../components/DashboardHeader"
 
 // styles
 import "../styles/dashboard.scss"
@@ -6,6 +7,7 @@ import "../styles/dashboard.scss"
 const Dashboard = () => {
   return (
     <div className="dashboard-page">
+      <DashboardHeader />
       <div className="dashboard-wrapper"></div>
     </div>
   )
