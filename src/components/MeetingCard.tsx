6f643b26d80626b3d28158b970660782c7ca75f3
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2025-04-15 12:09:02 +0100

    adding the types in the meetingcard component. adding styles next

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -1,7 +1,35 @@
 // styles
 import "../styles/meetingCard.scss"
-const MeetingCard = () => {
-  return <div>Hello</div>
+
+type MeetingCardProps = {
+  roomId: string
+  companyName: string
+  startTime: string
+  endTime: string
+  qrCodeUrl: string
+}
+const MeetingCard = ({
+  roomId,
+  companyName,
+  startTime,
+  endTime,
+  qrCodeUrl,
+}: MeetingCardProps) => {
+  return (
+    <div className="meeting-card">
+      <div className="card-left">
+        <strong>
+          {roomId} - {companyName}
+        </strong>
+        <span>
+          {startTime} - {endTime}
+        </span>
+      </div>
+      <div className="card-right">
+        <img src={qrCodeUrl} alt="QR Code" />
+      </div>
+    </div>
+  )
 }
 
 export default MeetingCard
